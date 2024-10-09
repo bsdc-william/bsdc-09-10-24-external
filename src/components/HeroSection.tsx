dc5a64aad1d00c9f06c44374cf67607cf6c8d613
nofrinth <will_banh@icloud.com>
2024-10-09 21:53:23 +0100

    Fixed transitions blud

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,33 +18,35 @@ export default function HeroSection() {
           <br />
           with your hands.
         </motion.h1>
-        <motion.p
+        <motion.div
           initial={{ opacity: 0.0, y: 40 }}
-          whileInView={{ opacity: 0.3, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{
             delay: 0.7,
             duration: 0.7,
             ease: "easeInOut",
           }}
-          className="mt-[2rem] max-w-[500px] opacity-30"
         >
-          Utilise futuristic concepts and resources to help take you, and your
-          ideas through the 5th dimention and into tommorow.
-        </motion.p>
-        <div className="mt-[1.5rem] flex gap-5">
-          <motion.a
-            href="/"
-            className="smooth_transition w-[50%] rounded-lg bg-blue-600/80 p-3 text-center hover:bg-blue-600"
-          >
-            Build the future you want.
-          </motion.a>
-          <motion.a
-            href="/"
-            className="smooth_transition g-gradient-to-r w-[30%] rounded-lg bg-neutral-950/70 p-3 text-center hover:bg-neutral-950/80"
-          >
-            About us
-          </motion.a>
-        </div>
+          <motion.p className="mt-[2rem] max-w-[500px] opacity-30">
+            Utilise futuristic concepts and resources to help take you, and your
+            ideas through the 5th dimention and into tommorow.
+          </motion.p>
+
+          <div className="mt-[1.5rem] flex gap-5">
+            <motion.a
+              href="/"
+              className="smooth_transition w-[50%] rounded-lg bg-blue-600/80 p-3 text-center hover:bg-blue-600"
+            >
+              Build the future you want.
+            </motion.a>
+            <motion.a
+              href="/"
+              className="smooth_transition g-gradient-to-r w-[30%] rounded-lg bg-neutral-950/70 p-3 text-center hover:bg-neutral-950/80"
+            >
+              About us
+            </motion.a>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
